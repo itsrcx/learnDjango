66c38dc55f3bd5798d3ff517f4cdc22ea1f38549
rcx <ramanchauhanxx@gmail.com>
2023-07-17 16:15:28 +0530

    MySQl DB added

--- learnDjango/learnDjango/settings.py
+++ learnDjango/learnDjango/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +78,11 @@ WSGI_APPLICATION = 'learnDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'blogging/my.cnf',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
@@ -121,7 +126,7 @@ STATIC_URL = 'static/'
 # bootstrap 4
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "blogging/static",
 ]
 
 
