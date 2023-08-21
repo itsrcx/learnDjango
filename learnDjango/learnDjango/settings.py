c06d24b5d7b7444ac7bb79afb27c7f48bb87e9d2
raman <ramanchauhanxx@gmail.com>
2023-08-22 00:27:57 +0530

    no comments

--- learnDjango/learnDjango/settings.py
+++ learnDjango/learnDjango/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# bootstrap 4
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
