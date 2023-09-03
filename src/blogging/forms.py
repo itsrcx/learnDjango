a161e5d21c0b669f10b0adf3d9acb08efa236e80
Raman Chauhan <102892407+itsrcx@users.noreply.github.com>
2023-09-04 01:23:48 +0530

    add and update post

--- src/blogging/forms.py
+++ src/blogging/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Post
 from django import forms
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth.models import User
@@ -22,3 +22,14 @@ class NewUserForm(UserCreationForm):
             user.save()
         return user
     
+class AddPost(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','author','content','status')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control','placeholder':'Title size must not exceed 150 characters.'}),
+            'author': forms.Select(attrs={'class':'form-control'}),
+            'content': forms.Textarea(attrs={'class':'form-control'}),
+            'status': forms.Select(attrs={'class':'form-control'})
+        }
